b23c9fb3cf59c6c8860e545f8e9faa6f5845303d
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-10-16 18:54:18 -0300

    Finalizado Árvore Binária

--- Tree.pas
+++ Tree.pas
@@ -6,59 +6,68 @@ type ptrNode = ^node;
 							left, right: ptrNode;
 						end;
 							
-procedure initializate(var root:ptrNode);
+procedure initializate(var nodeElement:ptrNode);
 begin;
-	root := nil;
+	nodeElement := nil;
 end;
 
-procedure writeNode(root: ptrNode);
+procedure preOrdemTree(nodeElement: ptrNode);
 begin;
-	writeln(root^.value);
-end;
-
-procedure preOrdemTree(root: ptrNode);
-begin;
-	while (root <> nil) do
+	if (nodeElement <> nil) then
 		begin;
-			writenode(root);
-			preOrdemTree(root^.left);
-			preOrdemTree(root^.right);
+			writeln(nodeElement^.value);
+			preOrdemTree(nodeElement^.left);
+			preOrdemTree(nodeElement^.right);
 		end;
 end;
 
-procedure ordemTree(root: ptrNode);
+procedure ordemTree(nodeElement: ptrNode);
 begin;
-	while (root <> nil) do
+	if (nodeElement <> nil) then
 		begin;
-			ordemTree(root^.left);
-			writenode(root);
-			ordemTree(root^.right);
+			ordemTree(nodeElement^.left);
+			writeln(nodeElement^.value);
+			ordemTree(nodeElement^.right);
 		end;
 end;
 
-procedure posOrdemTree(root: ptrNode);
+procedure posOrdemTree(nodeElement: ptrNode);
 begin;
-	while (root <> nil) do
+	if (nodeElement <> nil) then
 		begin;
-			posOrdemTree(root^.left);
-			writenode(root);
-			posOrdemTree(root^.right);
+			posOrdemTree(nodeElement^.left);
+			posOrdemTree(nodeElement^.right);
+			writeln(nodeElement^.value);
 		end;
 end;
 
-procedure addNode(var root: ptrNode; value: integer); 
+procedure showLeafs(nodeElement: ptrNode);
 begin;
-	if (root <> nil) then
+	if (nodeElement <> nil) then
 		begin;
-			if (value > root^.value) then
+			if ((nodeElement^.left = nil) and (nodeElement^.right = nil)) then
 				begin;
-					addNode(root^.right, value);
+					writeln(nodeElement^.value);
+				end;
+				
+			showLeafs(nodeElement^.left);
+			showLeafs(nodeElement^.right);
+		end;
+end;
+
+procedure addNode(var nodeElement: ptrNode; value: integer); 
+begin;
+	if (nodeElement <> nil) then
+		begin;
+			if (value > nodeElement^.value) then
+				begin;
+					addNode(nodeElement^.right, value);
 				end	 
-			else if (value < root^.value) then
+			else if (value < nodeElement^.value) then
 				begin;
-					addNode(root^.left, value);
+					addNode(nodeElement^.left, value);
 				end
-			else if (value = root^.value) then
+			else if (value = nodeElement^.value) then
 				begin;
 					writeln('O valor já está presente na árvore');
 				end
@@ -66,8 +75,10 @@ begin;
 		end
 	else
 		begin;
-			new(root);
-			root^.value := value;
+			new(nodeElement);
+			nodeElement^.value := value;
+			nodeElement^.left := nil;
+			nodeElement^.right := nil;
 		end;
 end;
 
@@ -82,8 +93,9 @@ begin
 		writeln('1 - Ler em Pré-Ordem');
 		writeln('2 - Ler em Ordem');
 		writeln('3 - Ler em Pós-Ordem');
-		writeln('4 - Adicionar elemento');
-		writeln('5 - Finalizar');
+		writeln('4 - Ler folhas');
+		writeln('5 - Adicionar elemento');
+		writeln('6 - Finalizar');
 		write('Escolha uma opção: ');
 		readln(op);
 		clrscr;
@@ -92,7 +104,8 @@ begin
 			1: preOrdemTree(root);
 			2: ordemTree(root);
 			3: posOrdemTree(root);
-			4: 
+			4: showLeafs(root);
+			5: 
 				begin;
 					writeln('Informe o valor que deseja inserir');
 					readln(value);
@@ -100,13 +113,13 @@ begin
 				end;
 		end;
 
-		if op <> 5 then
+		if op <> 6 then
 		begin
 			writeln;
 			write('Pressione ENTER para continuar...');
 			readln;
 		end;
-	until op = 5;
+	until op = 6;
 end;
 
 Begin
